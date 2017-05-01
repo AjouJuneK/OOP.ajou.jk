f5bdcccc5f3ab1418f549e231f8530fd02b95e66
namori3 <namori3@outlook.com>
2017-05-01 11:31:22 +0900

    Hello guys

---
+++ lab07/src/kr/ac/ajou/lab07/task01/ProductTest.java
@@ -0,0 +1,29 @@
+package kr.ac.ajou.lab07.task01;
+
+public class ProductTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void testProduct()
+	{
+		
+	}
+	
+	public void testDataStorage(DataStorage ds)
+	{
+		
+	}
+	/*
+	public void testNetworked(Networked n)
+	{
+		
+	}
+
+	public void testCooker(Cooker c)
+	{
+		
+	} */
+}
---
+++ lab07/src/kr/ac/ajou/lab07/task01/SmartTV.java
@@ -0,0 +1,40 @@
+package kr.ac.ajou.lab07.task01;
+
+public class SmartTV extends Product {
+
+	private int channel;
+	
+	public int getChannel() {
+		return channel;
+	}
+
+	public void setChannel(int channel) {
+		if(channel >= 0)
+			this.channel = channel;
+		else
+			System.out.println("Channel input ERROR!");
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "I am Smart TV";
+	}
+	
+	public boolean isConnected()
+	{
+		System.out.println("TV is connected");
+		return true;
+	}
+	
+	public double maxSpeed()
+	{
+		System.out.println("TV return integer 30");
+		return 30;
+	}
+	
+
+	
+	
+
+}
