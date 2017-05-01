1532418c92579e82cf310638320aaccb3eba9391
leejeep <leejeep@DESKTOP-MUHE75J>
2017-05-01 16:09:07 +0900

    interface Networked class Labtop

---
+++ lab07/src/kr/ac/ajou/lab07/task01/Laptop.java
@@ -0,0 +1,63 @@
+package kr.ac.ajou.lab07.task01;
+
+public class Laptop extends Product implements Networked {
+
+	private double totalCapacity;
+	private double usedCapacity;
+	
+	
+	public Laptop(double price) {
+		super(price);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public double getFreeCapacity(){
+		
+		return 0;
+	}
+	
+	public void format(){
+		
+		
+	}
+	
+	
+	@Override
+	public boolean isConnected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double maxSpeed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "Laptop";
+	}
+	
+	
+	
+	public double getTotalCapacity() {
+		return totalCapacity;
+	}
+
+	public void setTotalCapacity(double totalCapacity) {
+		this.totalCapacity = totalCapacity;
+	}
+
+	public double getUsedCapacity() {
+		return usedCapacity;
+	}
+
+	public void setUsedCapacity(double usedCapacity) {
+		this.usedCapacity = usedCapacity;
+	}
+
+
+
+}
---
+++ lab07/src/kr/ac/ajou/lab07/task01/Networked.java
@@ -0,0 +1,10 @@
+package kr.ac.ajou.lab07.task01;
+
+public interface Networked {
+	
+	abstract public boolean isConnected();
+	
+	abstract public double maxSpeed();
+	
+
+}
